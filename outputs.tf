a42cb69377391c87624b21f45d3cad1af5b80628
NotMaharshi <maharshi.bhatnagar@clouddrove.com>
2025-05-14 15:31:47 +0530

    feat: Added tags module with new format and workflows

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,43 @@
 ##-----------------------------------------------------------------------------
-## Outputs
+## Labels - Designed to generate consistent label names and tags for resources. You can use terraform-labels to implement a strict naming convention.
 ##-----------------------------------------------------------------------------
+
+output "id" {
+  value       = local.id
+  description = "Disambiguated ID."
+}
+
+output "name" {
+  value       = local.name
+  description = "Normalized name."
+}
+
+output "repository" {
+  value       = local.repository
+  description = "Terraform current module repo"
+}
+
+output "business_unit" {
+  value       = local.business_unit
+  description = "Normalized business_unit"
+}
+
+output "environment" {
+  value       = local.environment
+  description = "Normalized environment"
+}
+
+output "attributes" {
+  value       = local.attributes
+  description = "Normalized attributes."
+}
+
+output "tags" {
+  value       = local.tags
+  description = "Normalized Tag map."
+}
+
 output "label_order" {
   value       = local.label_order
-  description = "Label order."
+  description = "Normalized Tag map."
 }
