97b2668ae80b6f0857ed92da0b1f60774116de80
Maharshi Bhatnagar <159241848+NotMaharshi@users.noreply.github.com>
2025-05-21 17:32:06 +0530

    feat: Added tags module with new format and workflows
    * feat: Added tags module with new format and workflows

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -1,7 +1,21 @@
+
 provider "azurerm" {
   features {}
 }
 
 ##-----------------------------------------------------------------------------
-## Resources
+## Labels module call.
 ##-----------------------------------------------------------------------------
+module "labels" {
+  source        = "./../.."
+  name          = "labels"
+  environment   = "test"
+  location      = "northeurope"
+  label_order   = ["name", "environment", "location"]
+  managedby     = "terraform-az-modules"
+  business_unit = "Corp"
+  attributes    = ["private"]
+  extra_tags = {
+    Application = "test-app"
+  }
+}
--- examples/complete/outputs.tf
+++ examples/complete/outputs.tf
@@ -1,3 +1,12 @@
 ##-----------------------------------------------------------------------------
 ## Outputs
 ##-----------------------------------------------------------------------------
+output "id" {
+  value       = module.labels.id
+  description = "Disambiguated ID."
+}
+
+output "tags" {
+  value       = module.labels.tags
+  description = "Normalized Tag map."
+}
